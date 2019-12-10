0b6b82a86084233ca5c4863d952d7ef83cfd59ab
onefreeguy <57221515+onefreeguy@users.noreply.github.com>
2019-12-10 11:02:06 +0800

    create project

---
+++ resources/views/backend/includes/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="app-footer">
+        <div class="d-block w-100 text-center">
+                <strong>@lang('labels.general.copyright') &copy; {{ date('Y') }}
+                        <a>UtekSolution</a>
+                </strong> @lang('strings.backend.general.all_rights_reserved') <p class="float-right mb-0">
+                        </p></div>
+
+</footer>
